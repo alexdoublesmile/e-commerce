ec52076159d25e71d9b27347724354347ef80caa
Александр Плохой <alexplohoy@mail.ru>
2024-04-07 15:11:31 +0300

    Initial commit

---
+++ commerce-admin/src/main/java/com/example/commerceadmin/CommerceAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.commerceadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceAdminApplication.class, args);
+	}
+
+}
---
+++ commerce-admin/src/test/java/com/example/commerceadmin/CommerceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commerceadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
