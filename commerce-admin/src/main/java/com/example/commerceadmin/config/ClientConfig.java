0da915cd89942069266b36dbf69b71a5216ce1a9
Александр Плохой <alexplohoy@mail.ru>
2024-04-09 12:12:12 +0300

    add admin to client basic auth

--- commerce-admin/src/main/java/com/example/commerceadmin/config/ClientConfig.java
+++ commerce-admin/src/main/java/com/example/commerceadmin/config/ClientConfig.java
@@ -4,6 +4,7 @@ import com.example.commerceadmin.client.RestProductClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,9 +12,14 @@ public class ClientConfig {
 
     @Bean
     public RestProductClient restProductClient(
-            @Value("${commerce-service.url:http://localhost:8080}") String serviceBaseUri) {
+            @Value("${commerce-service.host}") String host,
+            @Value("${commerce-service.port}") String port,
+            @Value("${commerce-service.username}") String username,
+            @Value("${commerce-service.password}") String password
+    ) {
         return new RestProductClient(RestClient.builder()
-                .baseUrl(serviceBaseUri)
+                .baseUrl(host + ":" + port)
+                .requestInterceptor(new BasicAuthenticationInterceptor(username, password))
                 .build());
     }
 }
