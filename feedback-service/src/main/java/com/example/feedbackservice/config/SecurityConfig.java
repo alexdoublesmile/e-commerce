820ae421919c6c90431e6c84650bde791c4cf7e8
Александр Плохой <alexplohoy@mail.ru>
2024-04-15 19:30:27 +0300

    add feedback service security

---
+++ feedback-service/src/main/java/com/example/feedbackservice/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.feedbackservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+        return http
+                .authorizeExchange(customizer -> customizer.anyExchange().authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .oauth2ResourceServer(customizer -> customizer.jwt(withDefaults()))
+                .build();
+    }
+}
