a1552ff636874e2a919338be017636affc41888c
Александр Плохой <alexplohoy@mail.ru>
2024-04-16 18:56:24 +0300

    add user identity view

--- customer-service/src/main/java/com/example/customerservice/model/entity/Review.java
+++ customer-service/src/main/java/com/example/customerservice/model/entity/Review.java
@@ -2,6 +2,6 @@ package com.example.customerservice.model.entity;
 
 import java.util.UUID;
 
-public record Review(UUID id, Long productId, Integer rating, String text) {
+public record Review(UUID id, Long productId, Integer rating, String text, String userId) {
 
 }
